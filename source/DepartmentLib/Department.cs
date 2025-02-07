a20b03d62d5a2b20fed82960ed768368746b7a16
MEDO63B <76916764+MEDO63B@users.noreply.github.com>
2025-02-08 00:09:30 +0200

    Test: Start testing on Employee and Department classes

---
+++ source/DepartmentLib/Department.cs
@@ -0,0 +1,24 @@
+﻿namespace DepartmentLib;
+
+public class Department
+{
+    public int ID { get; set; }
+    public string? Name { get; set; }
+
+    public Department() { }
+    public Department(int id, string name)
+    {
+        ID = id;
+        Name = name;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"    ID: {ID}");
+        Console.WriteLine($"    Name: {Name}");
+    }
+    public override string ToString()
+    {
+        return $"ID: {ID}, Name: {Name}";
+    }
+}
--- source/EmployeeLib/Employee.cs
+++ source/EmployeeLib/Employee.cs
@@ -1,6 +1,47 @@
-﻿namespace EmployeeLib;
+﻿using DepartmentLib;
+namespace EmployeeLib;
 
 public class Employee
 {
-    
+    public int ID { get; set; }
+    public string? Name { get; set; }
+    public int? DepartmentID { get; set; }
+
+    public Department? Department { get; set; }
+
+    public Employee() { }
+
+    public Employee(int id, string name)
+    {
+        ID = id;
+        Name = name;
+    }
+
+    public Employee(int id, string name, int? departmentID) : this(id, name)
+    {
+        if (departmentID != null)
+        {
+            DepartmentID = departmentID;
+        }
+    }
+
+    public Employee(int id, string name, Department? department) : this(id, name)
+    {
+        if (department != null)
+        {
+            Department = department;
+            DepartmentID = department?.ID;
+        }
+    }
+
+    public void Display()
+    {
+        Console.WriteLine($"    ID: {ID}");
+        Console.WriteLine($"    Name: {Name}");
+        Console.WriteLine($"    Department: {Department.Name}");
+    }
+    public override string ToString()
+    {
+        return $"ID: {ID}, Name: {Name}, DepartmentID: {DepartmentID}";
+    }
 }
